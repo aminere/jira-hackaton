5d2db746760e8f648db5f7f95dd8f1c0f702c487
Amine Rehioui <amine@franticsoftware.com>
2022-12-25 20:54:23 +0100

    Terrain wip

--- src/planet.ts
+++ src/planet.ts
@@ -4,6 +4,9 @@ import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 import { Terrain } from './terrain';
+import { PerlinNoise } from './perlin-noise';
+
+import { GUI } from 'dat.gui';
 
 export class Planet extends THREE.Scene {
 
@@ -30,12 +33,53 @@ export class Planet extends THREE.Scene {
         // const obj2 = await this.createObject(mtl);
         // obj2.position.set(1, -1, 0);
         // this.add(this.obj);
-        // this.add(obj2);
+        // this.add(obj2);        
+
+        // procedural texture
+        const resolution = 64;        
+        const pixels = new Uint8Array(resolution * resolution * 4);
+        const stride = resolution * 4;
+        for ( let y = 0; y < resolution; y ++ ) {
+            for ( let x = 0; x < resolution; x ++ ) {
+                const index = y * stride + x * 4;
+                const noise = PerlinNoise.get2DNoise(x, y, 6);
+                const color = new THREE.Color(noise, noise, noise);
+                const r = Math.floor( color.r * 255 );
+                const g = Math.floor( color.g * 255 );
+                const b = Math.floor( color.b * 255 );
+                pixels[ index ] = r;
+                pixels[ index + 1] = g;
+                pixels[ index + 2] = b;
+                pixels[ index + 3] = 255;
+            }
+        }
+
+        const texture = new THREE.DataTexture(pixels, resolution, resolution, THREE.RGBAFormat);
+        texture.minFilter = THREE.LinearFilter;
+        texture.magFilter = THREE.LinearFilter;
+        texture.needsUpdate = true;
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshBasicMaterial( {
+            color: 0xffffff,
+            // map: new THREE.TextureLoader().load('assets/noise.png'),
+            map: texture
+        } );
+        const cube = new THREE.Mesh( geometry, material );
+        this.add( cube );
+
         await new Promise(resolve => setTimeout(resolve, 1));
-        this.add(new Terrain({
-            cellSize: 1,
-            resolution: 50
-        }));
+
+        const terrain = new Terrain({
+            cellSize: .5,
+            resolution: 32
+        });
+
+        const gui = new GUI();
+        const matFolder = gui.addFolder('Material');
+        matFolder.add(terrain.material, 'wireframe');
+        matFolder.open();        
+
+        this.add(terrain);
         this.dispatchEvent({ type: "ready" });
     }
 
--- src/terrain.ts
+++ src/terrain.ts
@@ -1,5 +1,6 @@
 
 import * as THREE from 'three';
+import { PerlinNoise } from './perlin-noise';
 
 interface ITerrainOptions {
     cellSize: number;
@@ -13,19 +14,27 @@ export class Terrain extends THREE.Mesh {
 
         const vertexStride = options.resolution + 1;
         const vertexCount = Math.pow(vertexStride, 2);
-        console.log({vertexCount});
         const vertices = new Float32Array(vertexCount * 3);
         const normals = new Float32Array(vertexCount * 3);      
-        
+        const colors = new Float32Array(vertexCount * 3);
+
         const normal = new THREE.Vector3(0, 1, 0);
         const size = options.cellSize * options.resolution;
         let start = new THREE.Vector3(size / 2, 0, -size / 2);
         let point = start.clone();
+        const color = new THREE.Color();
         let vertexIndex = 0;
+
         for (let y = 0; y < vertexStride; y++) {
             for (let x = 0; x < vertexStride; x++) {
+                const c = PerlinNoise.get2DNoise(x, y, 4);
+                point.y = c;
+
                 point.toArray(vertices, vertexIndex);
                 normal.toArray(normals, vertexIndex);
+
+                color.setRGB(c, c, c);
+                color.toArray(colors, vertexIndex);
                 vertexIndex += 3;
                 point.x -= options.cellSize;
             }
@@ -35,11 +44,11 @@ export class Terrain extends THREE.Mesh {
 
         geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
         geometry.setAttribute('normal', new THREE.BufferAttribute(normals, 3));
+        geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
 
         const indices = Array.from(new Array(vertexCount))
             .reduce((acc, _, i) => {
                 if ((i + 1) % vertexStride === 0) {
-                    console.log(`skipping ${i}`);
                     return acc;
                 }
                 return [
@@ -47,11 +56,11 @@ export class Terrain extends THREE.Mesh {
                     i + 0, i + 1, i + vertexStride,
                     i + 1, i + vertexStride + 1, i + vertexStride
                 ];
-            }, []);        
+            }, []);
 
-        console.log(indices);
-        geometry.setIndex(indices);
-        const material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+        geometry.setIndex(indices);        
+        const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+        material.vertexColors = true;
         super(geometry, material);
     }
 }
