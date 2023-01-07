54a88db5ff71a378893e939e235d3e7f107674e8
Amine Rehioui <amine@franticsoftware.com>
2023-01-07 18:26:02 +0100

    needed for camera zoom

--- src/utils.ts
+++ src/utils.ts
@@ -58,5 +58,15 @@ export class Utils {
         screenPos.y = -(screenPos.y - 1) / 2 * clientHeight;
         return screenPos;
     }
+
+    public static getWheelDelta(delta: number, deltaMode: number) {
+        if (deltaMode === 1) { // DOM_DELTA_LINE
+            return delta * 32; // approximation, supposed to be the font size
+        } else if (deltaMode === 2) { // DOM_DELTA_PAGE
+            return delta * 32 * 10; // approximation, supposed to be the 'page' size whatever the fuck this is
+        } else {
+            return delta; // DOM_DELTA_PIXEL
+        }
+    } 
 }
 
