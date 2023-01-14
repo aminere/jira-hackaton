41693efd014dffcb1a6798de51343231c64dce65
Amine Rehioui <amine@franticsoftware.com>
2023-01-14 16:53:55 +0100

    Forge integration

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,4 @@
-import { GUI } from "dat.gui";
-import type { Camera, Object3D } from "three";
-
-export interface IContext {
-    camera: Camera;
-    domElement: HTMLElement;
-    debugUI: GUI;
-}
-
-export interface ISeed {
-    angle: number;
-    object: Object3D;
-    jiraTaskId: string;
-}
+export const RESOLVERS = {
+    GET_PROJECTS: 'GET projects',
+    GET_SPACES: 'GET spaces',
+};
